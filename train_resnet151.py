e23242df47f70a862ef823ded8502f534311672f
mohit5tech <mohit.kumar@5techlab.com>
2025-01-14 10:37:37 +0530

    ResNet152V2
    ResNet152V2

---
+++ train_resnet151.py
@@ -0,0 +1,190 @@
+import os
+import time
+import shutil
+import pathlib
+import itertools
+
+# import data handling tools
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_style('darkgrid')
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# import Deep learning Libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout, BatchNormalization
+from tensorflow.keras import regularizers
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Dropout, Flatten, BatchNormalization
+from tensorflow.keras.optimizers import Adamax
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras import regularizers
+
+# Ignore Warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+print ('All modules loaded')
+
+#####data preprocessing 
+data_dir2 = 'DATASET'
+data_dir = 'chest_xray/train'
+
+
+filepaths = []
+labels = []
+
+folds = os.listdir(data_dir)
+# folds.remove('NORMAL')
+for fold in folds:
+    foldpath = os.path.join(data_dir, fold)
+    filelist = os.listdir(foldpath)
+    for file in filelist:
+        fpath = os.path.join(foldpath, file)
+        filepaths.append(fpath)
+        labels.append(fold)
+        
+        
+folds = ['Pleural' , 'Cardiomegaly']
+# folds = os.listdir(data_dir)
+# folds.remove('NORMAL')
+for fold in folds:
+    foldpath = os.path.join(data_dir2, fold)
+    filelist = os.listdir(foldpath)
+    for file in filelist:
+        fpath = os.path.join(foldpath, file)
+        filepaths.append(fpath)
+        labels.append(fold)
+
+# Concatenate data paths with labels into one dataframe
+Fseries = pd.Series(filepaths, name= 'filepaths')
+Lseries = pd.Series(labels, name='labels')
+df = pd.concat([Fseries, Lseries], axis= 1)
+
+# train dataframe
+train_df, dummy_df = train_test_split(df,  train_size= 0.8, shuffle= True, random_state= 123)
+
+# valid and test dataframe
+valid_df, test_df = train_test_split(dummy_df,  train_size= 0.6, shuffle= True, random_state= 123)
+
+
+# croped image size
+batch_size = 64
+img_size = (224, 224)
+channels = 3
+img_shape = (img_size[0], img_size[1], channels)
+
+# Recommended : use custom function for test data batch size, else we can use normal batch size.
+ts_length = len(test_df)
+test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length%n == 0 and ts_length/n <= 80]))
+test_steps = ts_length // test_batch_size
+
+# This function which will be used in image data generator for data augmentation, it just take the image and return it again.
+def scalar(img):
+    return img
+
+tr_gen = ImageDataGenerator(preprocessing_function= scalar)
+ts_gen = ImageDataGenerator(preprocessing_function= scalar)
+
+train_gen = tr_gen.flow_from_dataframe( train_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                    color_mode= 'rgb', shuffle= True, batch_size= batch_size)
+
+valid_gen = ts_gen.flow_from_dataframe( valid_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                    color_mode= 'rgb', shuffle= True, batch_size= batch_size)
+
+# Note: we will use custom test_batch_size, and make shuffle= false
+test_gen = ts_gen.flow_from_dataframe( test_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                    color_mode= 'rgb', shuffle= False, batch_size= test_batch_size)
+
+
+g_dict = train_gen.class_indices      # defines dictionary {'class': index}
+classes = list(g_dict.keys())       # defines list of dictionary's kays (classes), classes names : string
+images, labels = next(train_gen)      # get a batch size samples from the generator
+
+
+from tensorflow.keras.applications import ResNet50,ResNet152
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adamax
+
+# Define image size, channels, and number of classes
+img_size = (224, 224)
+channels = 3
+class_count = len(list(train_gen.class_indices.keys()))
+
+# Load ResNet50 pre-trained model
+base_model = ResNet152(include_top=False, weights='imagenet', input_shape=(img_size[0], img_size[1], channels))
+
+# Add Global Average Pooling layer
+x = GlobalAveragePooling2D()(base_model.output)
+
+# Output layer
+output = Dense(class_count, activation='softmax')(x)
+
+# Define new model
+model = Model(inputs=base_model.input, outputs=output)
+
+# Freeze the base model's layers
+base_model.trainable = False
+
+# Compile the model
+model.compile(Adamax(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Print model summary
+model.summary()
+class_names = list(train_gen.class_indices.keys())
+print(class_names)
+batch_size = 64   # set batch size for training
+epochs = 12  # number of all epochs in training
+
+history = model.fit(x= train_gen, epochs= epochs, verbose= 1, validation_data= valid_gen, 
+                    validation_steps= None, shuffle= False)
+
+test_loss, test_accuracy = model.evaluate(test_gen)
+print("Test Accuracy:", test_accuracy)
+
+# Define needed variables
+tr_acc = history.history['accuracy']
+tr_loss = history.history['loss']
+val_acc = history.history['val_accuracy']
+val_loss = history.history['val_loss']
+index_loss = np.argmin(val_loss)
+val_lowest = val_loss[index_loss]
+index_acc = np.argmax(val_acc)
+acc_highest = val_acc[index_acc]
+Epochs = [i+1 for i in range(len(tr_acc))]
+loss_label = f'best epoch= {str(index_loss + 1)}'
+acc_label = f'best epoch= {str(index_acc + 1)}'
+
+ts_length = len(test_df)
+test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length%n == 0 and ts_length/n <= 80]))
+test_steps = ts_length // test_batch_size
+
+train_score = model.evaluate(train_gen, steps= test_steps, verbose= 1)
+valid_score = model.evaluate(valid_gen, steps= test_steps, verbose= 1)
+test_score = model.evaluate(test_gen, steps= test_steps, verbose= 1)
+
+print("Train Loss: ", train_score[0])
+print("Train Accuracy: ", train_score[1])
+print('-' * 20)
+print("Validation Loss: ", valid_score[0])
+print("Validation Accuracy: ", valid_score[1])
+print('-' * 20)
+print("Test Loss: ", test_score[0])
+print("Test Accuracy: ", test_score[1])
+
+preds = model.predict(test_gen)
+y_pred = np.argmax(preds, axis=1)
+
+print(classification_report(test_gen.classes, y_pred, target_names= classes))
+model.save('chest_x-raysResNet152-V2_12.h5')
